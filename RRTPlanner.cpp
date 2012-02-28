e9e79f90b0791a9e218358c8b73e55300e6320ee
Daniel Madsen <paddington33@gmail.com>
2012-02-28 17:54:18 +0100

    Latest

--- RRTPlanner.cpp
+++ RRTPlanner.cpp
@@ -7,6 +7,7 @@
 
 #include "RRTPlanner.h"
 #include "rw/math.hpp"
+#include "rw/math/Math.hpp"
 
 RRTPlanner::RRTPlanner() {
 
@@ -19,4 +20,29 @@ RRTPlanner::~RRTPlanner() {
 void RRTPlanner::Plan(rw::math::Q qInit, rw::math::Q qGoal)
 {
 
+	rw::common::Ptr<rw::models::WorkCell> workcell = robWorkStudio->getWorkCell();
+	rw::common::Ptr<rw::models::Device> device = workcell->findDevice("KukaKr16");
+
+	CollisionStrategy::Ptr cdstrategy = ProximityStrategyFactory::makeCollisionStrategy("PQP");
+	CollisionDetector::Ptr collisionDetector = new CollisionDetector(workcell, cdstrategy);
+
+	rw::pathplanning::QConstraint::Ptr constraint = rw::pathplanning::QConstraint::make(
+			collisionDetector, device, state);
+
+	double epsilon = 1e-4;
+
+	RRT* aTree = new RRT();
+	RRT* bTree = new RRT();
+
+	RRT* currentTree = aTree;
+
+	int K = 100; //Number of tries
+
+	for(int i = 0; i < K ;i++)
+	{
+		rw::math::Q randQ = rw::math::Math::ranQ(device->getBounds());
+
+
+		randQ -
+	}
 }
