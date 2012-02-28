822f794ea5688c872f8bb91f7c184a293ba5fb37
Daniel Madsen <paddington33@gmail.com>
2012-02-28 17:25:09 +0100

    RRTPlanner class

---
+++ RRTPlanner.cpp
@@ -0,0 +1,17 @@
+/*
+ * RRTPlanner.cpp
+ *
+ *  Created on: Feb 28, 2012
+ *      Author: daniel
+ */
+
+#include "RRTPlanner.h"
+
+RRTPlanner::RRTPlanner() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RRTPlanner::~RRTPlanner() {
+	// TODO Auto-generated destructor stub
+}
---
+++ RRTPlanner.h
@@ -0,0 +1,17 @@
+/*
+ * RRTPlanner.h
+ *
+ *  Created on: Feb 28, 2012
+ *      Author: daniel
+ */
+
+#ifndef RRTPLANNER_H_
+#define RRTPLANNER_H_
+
+class RRTPlanner {
+public:
+	RRTPlanner();
+	virtual ~RRTPlanner();
+};
+
+#endif /* RRTPLANNER_H_ */
