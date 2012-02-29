1f39f6966d9f8d5393f0b065aa45cea4e1271978
Daniel Madsen <paddington33@gmail.com>
2012-02-29 17:58:40 +0100

    Multi with new methods

--- RRTMTPlanner.cpp
+++ RRTMTPlanner.cpp
@@ -6,26 +6,44 @@
  */
 
 #include "RRTMTPlanner.h"
-#include "RRTPlanner.h"
 
-RRTMTPlanner::RRTMTPlanner(rws::RobWorkStudio* robWorkStudio) : RRTPlanner(robWorkStudio) {
-	std::cout << "I am a RRTMT Planner" << std::endl;
+RRTMTPlanner::RRTMTPlanner(rws::RobWorkStudio* robWorkStudio, int connectN) :
+	RRTPlanner(robWorkStudio) ,
+	_connectN(connectN)
+{
+
 }
 
 RRTMTPlanner::~RRTMTPlanner() {
 
 }
 
-std::list<rw::math::Q> RRTMTPlanner::plan(rw::math::Q qInit, rw::math::Q qGoal)
+rw::trajectory::QPath RRTMTPlanner::RRTMTPlanner::plan(rw::math::Q qInit, rw::math::Q qGoal)
 {
-	std::cout << "Planning with RRTMTPlanner" << std::endl;
-	std::list<rw::math::Q>* path = new std::list<rw::math::Q>();
 
+}
 
+rw::common::Ptr<RRTNode> RRTMTPlanner::extendTreeInRandomDirection(rw::common::Ptr<RRTNode> nodeClose)
+{
 
+}
 
+bool RRTMTPlanner::edgeCollisionDetection(rw::common::Ptr<RRTNode> nodeClose, rw::common::Ptr<RRTNode> nodeNew)
+{
 
-	return *path;
+}
 
+rw::common::Ptr<RRTNode> RRTMTPlanner::cloestNodeInAnyOtherTree(rw::common::Ptr<RRT> currentTree)
+{
+
+}
+
+void RRTMTPlanner::connect(rw::common::Ptr<RRT> currentTree, rw::common::Ptr<RRTNode> newNode, rw::common::Ptr<RRTNode> closestNode)
+{
+
+}
+
+rw::common::Ptr<RRT> RRTMTPlanner::swap(rw::common::Ptr<RRT> currentTree)
+{
 
 }
--- RRTMTPlanner.h
+++ RRTMTPlanner.h
@@ -2,19 +2,32 @@
  * RRTMTPlanner.h
  *
  *  Created on: Feb 29, 2012
- *      Author: daniel
+ *      Author: Daniel
  */
 
 #ifndef RRTMTPLANNER_H_
 #define RRTMTPLANNER_H_
 
 #include "RRTPlanner.h"
+#include "RRTNode.h"
+#include "RRT.h"
 #include <rws/RobWorkStudio.hpp>
+#include <rw/trajectory.hpp>
+#include <rw/common.hpp>
+
 
 class RRTMTPlanner: public RRTPlanner {
+private:
+	std::list<rw::common::Ptr<RRT> > _trees;
+	int _connectN;
 public:
-	RRTMTPlanner(rws::RobWorkStudio* robWorkStudio);
-	std::list<rw::math::Q> plan(rw::math::Q qInit, rw::math::Q qGoal);
+	RRTMTPlanner(rws::RobWorkStudio* robWorkStudio, int connectN = -1);
+	rw::trajectory::QPath plan(rw::math::Q qInit, rw::math::Q qGoal);
+	rw::common::Ptr<RRTNode> extendTreeInRandomDirection(rw::common::Ptr<RRTNode> nodeClose);
+	bool edgeCollisionDetection(rw::common::Ptr<RRTNode> nodeClose, rw::common::Ptr<RRTNode> nodeNew);
+	rw::common::Ptr<RRTNode> cloestNodeInAnyOtherTree(rw::common::Ptr<RRT> currentTree);
+	void connect(rw::common::Ptr<RRT> currentTree, rw::common::Ptr<RRTNode> newNode, rw::common::Ptr<RRTNode> closestNode);
+	rw::common::Ptr<RRT> swap(rw::common::Ptr<RRT> currentTree);
 	virtual ~RRTMTPlanner();
 };
 
