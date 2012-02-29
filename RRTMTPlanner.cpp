3d8c4f111d5cf191a2be9008ab85b262f3b167a4
Daniel Madsen <paddington33@gmail.com>
2012-02-29 20:14:36 +0100

    Danel

--- RRTMTPlanner.cpp
+++ RRTMTPlanner.cpp
@@ -12,14 +12,44 @@ RRTMTPlanner::RRTMTPlanner(rws::RobWorkStudio* robWorkStudio, int connectN) :
 	_connectN(connectN)
 {
 
+	_workcell = robWorkStudio->getWorkcell();
+	_device = _workcell->findDevice("KukaKr16");
+	_cdstrategy = rwlibs::proximitystrategies::ProximityStrategyFactory::makeCollisionStrategy("PQP");
+	_collisionDetector = new rw::proximity::CollisionDetector(_workcell, _cdstrategy);
+	_constraint = rw::pathplanning::QConstraint::make(_collisionDetector, _device, _workcell->getDefaultState());
 }
 
 RRTMTPlanner::~RRTMTPlanner() {
 
 }
 
-rw::trajectory::QPath RRTMTPlanner::RRTMTPlanner::plan(rw::math::Q qInit, rw::math::Q qGoal)
+void RRTMTPlanner::initTrees()
 {
+	_trees.push_back(new RRT(qInit));
+
+	for(int i = 1 ; i < _numberOfTrees -1 ; i++ )
+		_trees.push_back(new RRT(cFree->sample()));
+
+
+	if(_numberOfTrees != 1)
+		_trees.push_back(new RRT(qGoal));
+}
+
+rw::trajectory::QPath RRTMTPlanner::plan(rw::math::Q qInit, rw::math::Q qGoal)
+{
+	int K = 1000;
+
+	for(int i = 0;i < K; i++ )
+	{
+		rw::common::Ptr<RRT> currentTree = chooseTree(NULL);
+		rw::common::Ptr<RRTNode> newNode = extendTreeInRandomDirection(currentTree);
+		rw::common::Ptr<RRTNode> closestNode = cloestNodeInAnyOtherTree(currentTree,newNode);
+
+		if( ((rw::math::Q)(newNode->getValue() - closestNode->getValue())).norm2() < _d )
+			//Connect somehow
+
+		connect(currentTree,newNode,closestNode);
+	}
 
 }
 
--- RRTMTPlanner.h
+++ RRTMTPlanner.h
@@ -12,22 +12,37 @@
 #include "RRTNode.h"
 #include "RRT.h"
 #include <rws/RobWorkStudio.hpp>
+
+#include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+
 #include <rw/trajectory.hpp>
 #include <rw/common.hpp>
+#include <rw/pathplanning/QConstraint.hpp>
+
+
 
 
 class RRTMTPlanner: public RRTPlanner {
 private:
 	std::list<rw::common::Ptr<RRT> > _trees;
+
+	rw::common::Ptr<rw::models::WorkCell> _workcell;
+	rw::common::Ptr<rw::models::Device> _device;
+	rw::common::Ptr<rw::proximity::CollisionStrategy> _cdstrategy;
+	rw::common::Ptr<rw::proximity::CollisionDetector> _collisionDetector;
+	rw::common::Ptr<rw::pathplanning::QConstraint> _constraint;
+
 	int _connectN;
+	double _d; //Min distance to test connection
 public:
 	RRTMTPlanner(rws::RobWorkStudio* robWorkStudio, int connectN = -1);
+	void initTrees();
 	rw::trajectory::QPath plan(rw::math::Q qInit, rw::math::Q qGoal);
-	rw::common::Ptr<RRTNode> extendTreeInRandomDirection(rw::common::Ptr<RRTNode> nodeClose);
+	rw::common::Ptr<RRTNode> extendTreeInRandomDirection(rw::common::Ptr<RRT> currentTree);
 	bool edgeCollisionDetection(rw::common::Ptr<RRTNode> nodeClose, rw::common::Ptr<RRTNode> nodeNew);
 	rw::common::Ptr<RRTNode> cloestNodeInAnyOtherTree(rw::common::Ptr<RRT> currentTree);
 	void connect(rw::common::Ptr<RRT> currentTree, rw::common::Ptr<RRTNode> newNode, rw::common::Ptr<RRTNode> closestNode);
-	rw::common::Ptr<RRT> swap(rw::common::Ptr<RRT> currentTree);
+	rw::common::Ptr<RRT> chooseTree(rw::common::Ptr<RRT> currentTree);
 	virtual ~RRTMTPlanner();
 };
 
